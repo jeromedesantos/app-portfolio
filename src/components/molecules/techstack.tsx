a2547759ce2220a2da988fe538d609e1474d2f73
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-11-03 06:43:35 +0700

    carosel sementara

--- src/components/molecules/techstack.tsx
+++ src/components/molecules/techstack.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { Carousel } from "./carousel";
 
 export function TechStack() {
   return (
     <section
-      id={"Ex"}
+      id={"#techstack"}
       className="flex flex-col gap-10 pt-20 py-10 px-5 items-center bg-accent"
     >
       <div className="flex flex-col gap-5">
@@ -15,10 +16,12 @@ export function TechStack() {
           transition={{ duration: 0.5 }}
           className="text-5xl font-extrabold leading-tight tracking-tight duration-300"
         >
-          {"Tech Stack - Tools I Use Everyday"}
+          {"Tools I Use Everyday"}
         </motion.h1>
       </div>
-      <section id="tech-stack">{/* <StackCarousel /> */}</section>
+      <section id="tech-stack">
+        <Carousel />
+      </section>
     </section>
   );
 }
