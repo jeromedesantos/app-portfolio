7ce730de65c4ab625d29b86f30ca960085923bfa
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-11-04 14:52:42 +0700

    clean code

--- src/components/ui/icon-si.tsx
+++ src/components/ui/icon-si.tsx
@@ -1,5 +1,6 @@
 import { FC, SVGProps, useMemo } from "react";
 import dynamic from "next/dynamic";
+import { IconType } from "react-icons";
 
 interface DynamicSiIconProps extends SVGProps<SVGSVGElement> {
   name: string;
@@ -8,17 +9,15 @@ interface DynamicSiIconProps extends SVGProps<SVGSVGElement> {
 export const DynamicSiIcon: FC<DynamicSiIconProps> = ({ name, ...props }) => {
   const Icon = useMemo(() => {
     return dynamic(
-      () =>
-        import("react-icons/si").then((mod) => {
-          const icon = mod[name as keyof typeof mod];
-          if (!icon) {
-            console.warn(
-              `Icon with name "${name}" not found in react-icons/si.`
-            );
-            return () => null;
-          }
-          return icon;
-        }),
+      async () => {
+        const mod = await import("react-icons/si");
+        const icon = mod[name as keyof typeof mod] as IconType | undefined;
+        if (!icon) {
+          console.warn(`Icon with name "${name}" not found in react-icons/si.`);
+          return () => null;
+        }
+        return icon;
+      },
       {
         loading: () => <div style={{ width: 48, height: 48 }} />,
       }
